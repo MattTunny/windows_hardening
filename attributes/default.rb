620e4083e63883e587d4a77d6e7e5e44e080b8d4
Matt Tunny <matt.tunny@gmail.com>
2017-12-15 08:59:41 +1000

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,22 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
-default['NTLM_Harden'] = false
+default['cb_server_hardening']['enable_firewall'] = false
+default['cb_server_hardening']['enable_winrm'] = true
+default['cb_server_hardening']['force_windowsupdate'] = false
+default['cb_server_hardening']['harden_ntlm'] = false
+default['cb_server_hardening']['harden_winrm'] = false
+
+# ciphers
+default['cb_server_hardening']['ciphers']['disable_tls1.0'] = true
+default['cb_server_hardening']['ciphers']['disable_tls1.1'] = true
+default['cb_server_hardening']['ciphers']['disable_pct'] = true
+default['cb_server_hardening']['ciphers']['disable_ssl2'] = true
+default['cb_server_hardening']['ciphers']['disable_ssl3'] = true
+default['cb_server_hardening']['ciphers']['disable_des'] = true
+default['cb_server_hardening']['ciphers']['disable_null'] = true
+default['cb_server_hardening']['ciphers']['disable_RC240_128'] = true
+default['cb_server_hardening']['ciphers']['disable_RC256_128'] = true
+default['cb_server_hardening']['ciphers']['disable_RC440_128'] = true
+default['cb_server_hardening']['ciphers']['disable_RC456_128'] = true
+default['cb_server_hardening']['ciphers']['disable_RC464_128'] = true
